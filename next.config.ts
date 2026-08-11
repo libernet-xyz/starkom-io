2faf0dcc82ed734aa448d4aed370fa2152094ead
71104 <a71104@gmail.com>
2026-08-11 10:26:17 +0200

    Export as static website for GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  output: "export",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
